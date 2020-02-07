28cc292d3cc05788979a89db2867dec0c250cf0a
Edgar Luque <blog@anonim.cat>
2020-02-07 14:31:08 +0100

    update license in __init__.py

--- moneypy/__init__.py
+++ moneypy/__init__.py
@@ -4,7 +4,7 @@ A library to handle money.
 
 __version__ = "1.0.1"
 __author__ = 'Edgar Luque'
-__license__ = 'MIT'
+__license__ = 'GPL-3.0'
 
 from .currency import Currency
 from .money import Money
